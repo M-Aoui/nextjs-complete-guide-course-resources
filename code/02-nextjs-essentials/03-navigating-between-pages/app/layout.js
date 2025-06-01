03817e7d1c498a44d8f339a66b0e2a455b583af7
M-Aoui <mikangelo95@gmail.com>
2025-06-01 17:27:31 +0100

    Update layout.js

--- code/02-nextjs-essentials/03-navigating-between-pages/app/layout.js
+++ code/02-nextjs-essentials/03-navigating-between-pages/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 
+// important at least one ROOT layout, which comprises the various nested page.js's. Nested layouts may be added too.
 // metadata takes care of Header block.
 export const metadata = {
   title: 'NextJS Course App',

6ec3bd11936e3fdf8eef9f058e9c14e459635795
M-Aoui <mikangelo95@gmail.com>
2025-06-01 17:39:36 +0100

    Update header.js

--- code/02-nextjs-essentials/04-configuring-dynamic-routes/components/header.js
+++ code/02-nextjs-essentials/04-configuring-dynamic-routes/components/header.js
@@ -1,6 +1,7 @@
 export default function Header() {
   return (
     <>
+    // we create a new component, pasting in these two lines only from page.
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
     </>
